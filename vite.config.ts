51dd1a532498b236fa865d22f5b89442b52120f2
John Ferlito <johnf@inodes.org>
2026-01-28 11:56:48 +1100

    feat: use relative base so app can run anywhere

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: '', // Ensure everything is relative
   plugins: [
     tsConfigPaths(),
     tanstackStart(),
